37aa04c33f3c3a1836101b8742aace3e1220729e
DanilKob <kobzardanila@gmail.com>
2018-08-18 21:17:45 +0300

    Constants for working with db

---
+++ src/main/java/model/dao/statement/ColumName.java
@@ -0,0 +1,13 @@
+package model.dao.statement;
+
+public interface ColumName {
+    String USER_TABLE = "user";
+    String USER_ID = "id";
+    String USER_LOGIN = "login";
+    String USER_PASSWORD = "password";
+    String USER_FIRST_NAME = "first_name";
+    String USER_LAST_NAME = "last_name";
+    String USER_MIDDLE_NAME = "middle_name";
+    String USER_ROLE = "role";
+
+}
---
+++ src/main/java/model/dao/statement/Statements.java
@@ -0,0 +1,14 @@
+package model.dao.statement;
+
+public interface Statements {
+    String INSERT_USER_FIRST_LAST_MIDDLE_NAME_LOGIN_PASSWORD = "INSERT INTO "+ColumName.USER_TABLE+
+            "("
+            + ColumName.USER_FIRST_NAME+","
+            + ColumName.USER_LAST_NAME+","
+            + ColumName.USER_MIDDLE_NAME+","
+            + ColumName.USER_LOGIN+","
+            + ColumName.USER_PASSWORD+","
+            +")"
+            +"VALUES (?,?,?,?,?)";
+    String SELECT_USER_BY_ID = "";
+}
